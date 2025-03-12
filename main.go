b97d691209f0d588357348009f0006a0122da0bf
Ricardo Ferrari <ricardoaugusto.ferrari@gmail.com>
2025-03-12 19:42:22 -0300

    feat: function as return value

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ type transformationFunction func(int) int
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
 	fmt.Println(transformNumbers(numbers, addOne))
+	fmt.Println(transformNumbers(numbers, createMultiplyByANumberFunction(3)))
 }
 
 func transformNumbers(numbers []int, f transformationFunction) []int {
@@ -21,4 +22,10 @@ func transformNumbers(numbers []int, f transformationFunction) []int {
 
 func addOne(number int) int {
 	return number + 1
+}
+
+func createMultiplyByANumberFunction(multiplier int) transformationFunction {
+	return func(number int) int {
+		return number * multiplier
+	}
 }
