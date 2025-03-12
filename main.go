b19c2652b60c9a617bc2f7dc4100e20a5730692e
Ricardo Ferrari <ricardoaugusto.ferrari@gmail.com>
2025-03-12 19:34:45 -0300

    feat: transformation function
    Created a first class function that accepts another function as a transformation method

--- main.go
+++ main.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+type transformationFunction func(int) int
+
 func main() {
-	fmt.Println("Hello, World!")
+	numbers := []int{1, 2, 3, 4, 5}
+	fmt.Println(transformNumbers(numbers, addOne))
+}
+
+func transformNumbers(numbers []int, f transformationFunction) []int {
+	var result []int
+	for _, number := range numbers {
+		result = append(result, f(number))
+	}
+	return result
+}
+
+func addOne(number int) int {
+	return number + 1
 }
