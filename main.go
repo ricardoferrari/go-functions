efc951605c3e3313343a4d711721d8301ccef8aa
Ricardo A. Ferrari <ricardoaugusto.ferrari@gmail.com>
2025-03-16 18:16:42 -0300

    feat: variadic function

--- main.go
+++ main.go
@@ -10,8 +10,13 @@ func main() {
 	numbers := []int{1, 2, 3, 4, 5}
 	fmt.Println(transformNumbers(numbers, addOne))
 	fmt.Println(transformNumbers(numbers, createMultiplyByANumberFunction(3)))
+	variadicFunction(1, 2, 3, 4, 5)
+	variadicFunction(numbers...)
 }
 
+// This function takes a slice of numbers and a function that transforms a number
+// It returns a slice of numbers that have been transformed by the function
+// The function passed in is a higher order function
 func transformNumbers(numbers []int, f transformationFunction) []int {
 	var result []int
 	for _, number := range numbers {
@@ -24,8 +29,16 @@ func addOne(number int) int {
 	return number + 1
 }
 
+// This function returns a function that multiplies a number by the multiplier
+// The function returned is a closure that captures the multiplier variable
 func createMultiplyByANumberFunction(multiplier int) transformationFunction {
 	return func(number int) int {
 		return number * multiplier
+	}
+}
+
+func variadicFunction(numbers ...int) {
+	for _, number := range numbers {
+		fmt.Println(number)
 	}
 }
